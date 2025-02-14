e9e24397eda7ce8f66386899e9ebf1d140527715
annakmather <annakmather@gmail.com>
2025-02-14 16:24:45 -0600

    theme update

--- _site/assets/js/theme.js
+++ _site/assets/js/theme.js
@@ -1,1 +1,46 @@
-let toggleTheme=e=>{setTheme("dark"==e?"light":"dark")},setTheme=e=>{if(transTheme(),setHighlight(e),setGiscusTheme(e),e){document.documentElement.setAttribute("data-theme",e);let t=document.getElementsByTagName("table");for(let m=0;m<t.length;m++)"dark"==e?t[m].classList.add("table-dark"):t[m].classList.remove("table-dark");let m=document.getElementsByClassName("jupyter-notebook-iframe-container");for(let t=0;t<m.length;t++){let a=m[t].getElementsByTagName("iframe")[0].contentWindow.document.body;"dark"==e?(a.setAttribute("data-jp-theme-light","false"),a.setAttribute("data-jp-theme-name","JupyterLab Dark")):(a.setAttribute("data-jp-theme-light","true"),a.setAttribute("data-jp-theme-name","JupyterLab Light"))}}else document.documentElement.removeAttribute("data-theme");localStorage.setItem("theme",e),"undefined"!=typeof medium_zoom&&medium_zoom.update({background:getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color")+"ee"})},setHighlight=e=>{"dark"==e?(document.getElementById("highlight_theme_light").media="none",document.getElementById("highlight_theme_dark").media=""):(document.getElementById("highlight_theme_dark").media="none",document.getElementById("highlight_theme_light").media="")},setGiscusTheme=e=>{function t(e){const t=document.querySelector("iframe.giscus-frame");t&&t.contentWindow.postMessage({giscus:e},"https://giscus.app")}t({setConfig:{theme:e}})},transTheme=()=>{document.documentElement.classList.add("transition"),window.setTimeout(()=>{document.documentElement.classList.remove("transition")},500)},initTheme=e=>{if(null==e||"null"==e){const t=window.matchMedia;t&&t("(prefers-color-scheme: dark)").matches&&(e="dark")}setTheme(e)};initTheme(localStorage.getItem("theme"));
+const STORAGE_KEY = "theme";
+const THEME_ATTR  = "data-theme";
+const QUERY_KEY   = "(prefers-color-scheme: dark)";
+
+const themes = {
+  LIGHT: "light",
+  DARK: "dark",
+};
+
+initTheme();
+
+function initTheme() {
+  const savedTheme = localStorage.getItem(STORAGE_KEY);
+
+  if (savedTheme) {
+    // Storage theme
+    setTheme(savedTheme);
+  } else if (window.matchMedia && window.matchMedia(QUERY_KEY).matches) {
+    // system theme
+    setTheme(themes.DARK);
+  } else {
+    // Default theme
+    setTheme(themes.LIGHT);
+  }
+
+  // Watch for system theme changes
+  window.matchMedia(QUERY_KEY).addEventListener("change", (e) => {
+    const newTheme = e.matches ? themes.DARK : themes.LIGHT;
+    setTheme(newTheme);
+  });
+}
+
+function toggleTheme() {
+  const theme = getTheme();
+  const newTheme = theme === themes.DARK ? themes.LIGHT : themes.DARK;
+  setTheme(newTheme);
+  localStorage.setItem(STORAGE_KEY, newTheme);
+}
+
+function getTheme() {
+  return document.documentElement.getAttribute(THEME_ATTR);
+}
+
+function setTheme(value) {
+  document.documentElement.setAttribute(THEME_ATTR, value);
+}
---
+++ pages/about.md
@@ -0,0 +1,23 @@
+---
+layout: page
+title: About
+permalink: /about/
+weight: 3
+---
+
+# **About Me**
+
+Hi there! I’m Anna, a Chicago based data engineer and analytics engineer (and occassional data scientist). Currently, I work as a data engineer for SRAM, a bicycle components company. In the past I’ve worked as a consultant, helping progressive political and non profit organizations tackle their data problems. 
+
+Outside of work, I’m a passionate cyclist, focusing on distance and gravel events. I ride for, and am a founding member of The Dropettes. When I’m not out exploring midwestern roads, I can be found knitting and taking the occassional guitar class.
+                      
+I’d love to hear from you! Feel free to reach out via email.
+
+<div class="row">
+{% include about/skills.html title="Programming Skills" source=site.data.programming-skills %}
+{% include about/skills.html title="Other Skills" source=site.data.other-skills %}
+</div>
+
+<div class="row">
+{% include about/timeline.html %}
+</div>
